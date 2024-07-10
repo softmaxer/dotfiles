8421a1dedeebc726c2e5f50d470785fed18efccc
softmaxer <sriram.vadlamani@proton.me>
2024-07-10 17:33:30 +0200

    Add latest polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-if [ -z "$(pgrep -x polybar)" ]; then
-    BAR="main"
-    for m in $(polybar --list-monitors | cut -d":" -f1); do
-        MONITOR=$m polybar --reload $BAR &
-        sleep 1
-    done
-else
-    polybar-msg cmd restart
-fi
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar -c ~/.config/polybar/config.ini main &
